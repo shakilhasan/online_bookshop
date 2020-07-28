2fda7c24979e23774d6b219445d55971c5beaa59
shakilhasan <shakilhasan105268@gmail.com>
2020-07-28 12:33:14 +0600

    graphql Relay

--- book_info/schema.py
+++ book_info/schema.py
@@ -1,52 +1,35 @@
 # cookbook/ingredients/schema.py
 import graphene
-
-from graphene_django.types import DjangoObjectType
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from book_info.models import Book_info, Catagory
 
-class CatagoryType(DjangoObjectType):
+# Graphene will automatically map the Catagory model's fields onto the CatagoryNode.
+# This is configured in the CatagoryNode's Meta class (as you can see below)
+
+class CatagoryNode(DjangoObjectType):
     class Meta:
         model = Catagory
+        filter_fields = ['name', 'book_infos']
+        interfaces = (relay.Node, )
 
 
-class Book_infoType(DjangoObjectType):
+class Book_infoNode(DjangoObjectType):
     class Meta:
         model = Book_info
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'description': ['exact', 'icontains'],
+            'catagory': ['exact'],
+            'catagory__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
 
+class Query(graphene.ObjectType):
+    catagory = relay.Node.Field(CatagoryNode)
+    all_catagories = DjangoFilterConnectionField(CatagoryNode)
 
-class Query(object):
-    catagory = graphene.Field(CatagoryType, id=graphene.Int(), name=graphene.String())
-    book_info = graphene.Field(Book_infoType, id=graphene.Int(), name=graphene.String())
-    all_catagories = graphene.List(CatagoryType)
-    all_Book_infos = graphene.List(Book_infoType)
-
-    def resolve_all_catagories(self, info, **kwargs):
-        return Catagory.objects.all()
-
-    def resolve_all_book_infos(self, info, **kwargs):
-        # We can easily optimize query count in the resolve method
-        return Book_info.objects.select_related('catagory').all()
-
-    def resolve_catagory(self, info, **kwargs):
-      id = kwargs.get('id')
-      name = kwargs.get('name')
-
-      if id is not None:
-          return Catagory.objects.get(pk=id)
-
-      if name is not None:
-          return Catagory.objects.get(name=name)
-
-      return None
-
-    def resolve_book_info(self, info, **kwargs):
-      id = kwargs.get('id')
-      name = kwargs.get('name')
-
-      if id is not None:
-          return Book_info.objects.get(pk=id)
-
-      if name is not None:
-          return Book_info.objects.get(name=name)
-
-      return None
+    book_info = relay.Node.Field(Book_infoNode)
+    all_book_infos = DjangoFilterConnectionField(Book_infoNode)
